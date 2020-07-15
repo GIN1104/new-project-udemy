90d74d0f4cd37b6025608b37a9f8cd3c822d0fb3
Evgeny <starchenkogin@gmail.com>
2020-07-15 11:54:49 +0300

    objects

--- js.js
+++ js.js
@@ -5,11 +5,11 @@
 // Условия////
 /////////////
 
-if(4 == 9){
-    console.log("Ok!");
-}else{
- console.log('Error');
-}
+// if(4 == 9){
+//     console.log("Ok!");
+// }else{
+//  console.log('Error');
+// }
 
 // const num = 50;
 
@@ -59,54 +59,95 @@ let num = 50;
 // while (num < 55);
 
 
-for(let i = 1; i < 10; i++){
-    if(i === 6){
-        // break;
-        continue;
+// for(let i = 1; i < 10; i++){
+//     if(i === 6){
+//         // break;
+//         continue;
+//     }
+//         console.log(i); 
+//         num++; 
+// }
+
+// ///////////////
+// // Функции ////
+// ///////////////
+// let num1 = 50;
+// function showFirstMassage(text, age){
+//     console.log(text, age);
+//     // let num1 = 20;
+//     // console.log(num1);
+// }
+
+// showFirstMassage('Hello World!');
+// showFirstMassage(24);
+// showFirstMassage('Hello World!', 24);
+// // console.log(num1);
+
+
+// function calc(a, b){
+//     return ( a + b);
+
+// }
+// console.log(calc(4 , 3));
+// console.log(calc(5 , 6));
+// console.log(calc(10 , 6));
+
+// function ret(){
+//     let num = 50;
+//     return num;
+// }
+// const anohterNum = ret();
+// console.log(anohterNum);
+
+// const logger = function (){
+//   console.log("Hello!");
+// };
+// logger();
+
+// const calc1 = (a,b) => {return a + b;};
+// // console.log(calc1(4, 6));
+// console.log(calc1(4, 90));
+// console.log(calc1(6));
+
+
+const options = {
+    name: 'test',
+    width: 1024,
+    hieght: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function(){
+        console.log('Test');
     }
-        console.log(i); 
-        num++; 
-}
-
-///////////////
-// Функции ////
-///////////////
-let num1 = 50;
-function showFirstMassage(text, age){
-    console.log(text, age);
-    // let num1 = 20;
-     console.log(num1);
-}
-
-showFirstMassage('Hello World!');
-showFirstMassage(24);
-showFirstMassage('Hello World!', 24);
-console.log(num1);
-
-
-function calc(a, b){
-    return ( a + b);
-
-}
-console.log(calc(4 , 3));
-console.log(calc(5 , 6));
-console.log(calc(10 , 6));
-
-function ret(){
-    let num = 50;
-    return num;
-}
-const anohterNum = ret();
-console.log(anohterNum);
-
-const logger = function (){
-  console.log("Hello!");
 };
-logger();
+options.makeTest();
 
-const calc1 = (a,b) => {return a + b;};
-console.log(calc1(4, 6));
-console.log(calc1(4, 90));
-console.log(calc1(6));
+const {border,bg} = options.colors;
+
+
+//console.log(Object.keys(options).length);
+
+// console.log(options.name);
+
+// delete options.width;
+
+//  console.log(options);
+// let counter = 0;
+// for (let key in options){
+ 
+//     if(typeof(options[key]) == 'object'){
+//         for(let i in options[key]){
+//             console.log(`Svojsto ${i} has ${options[key][i]}`); 
+//             counter++;  
+//         }
+//     }else{
+//         console.log(`Svojsto ${key} has ${options[key]}`);
+//         counter++; 
+//     }
+// }
+
+// console.log(counter);
 
 
