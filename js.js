9f18953865215ba30af483f7560ebb898274463d
Evgeny <starchenkogin@gmail.com>
2020-07-16 09:41:22 +0300

    Arrey

--- js.js
+++ js.js
@@ -151,3 +151,19 @@ const {border,bg} = options.colors;
 // console.log(counter);
 
 
+
+const arr = [2, 3, 6, 8, 10];
+
+// arr.pop();
+// arr.push(12);
+
+// console.log(arr);
+
+// for ( let i=0; i< arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+for (let value of arr){
+    console.log(value);
+}
+
